1dd77e3120d8bb9fb4754f54d8303d0ef9f6a21b
Muskan Chauhan <chauhanmuskan291980@gmail.com>
2026-02-24 15:34:45 +0530

    add Post model and create_posts endpoint for post creation

--- main.py
+++ main.py
@@ -1,6 +1,14 @@
 from fastapi import FastAPI, Body
+from pydantic import BaseModel
+from typing import Optional 
 
 app = FastAPI()
+
+class Post(BaseModel):
+    title: str
+    content:str
+    publish: bool = True
+    rating: Optional[int] = None
 
 @app.get("/")
 async def root():
@@ -16,3 +24,13 @@ async def get_post():
 async def create_post(payload:dict = Body(...)):
     print(payload)
     return {"new_post":f"title {payload['title']} content {payload['content']}"}
+
+
+# title str , content str 
+@app.post("/createpost")
+def create_posts(new_post:Post):
+    print(new_post.title)
+    print(new_post.content)
+    print(new_post.publish)
+    print(new_post.dict())
+    return {"data":f"{new_post}"}
