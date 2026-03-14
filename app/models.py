2994909063a03636d6d69fe3932bd7060cebf118
Muskan Chauhan <chauhanmuskan291980@gmail.com>
2026-03-14 22:29:48 +0530

    Refactor User model to define schema and enforce constraints; add user attributes and server default for created_at

--- app/models.py
+++ app/models.py
@@ -14,3 +14,16 @@ class Post(Base):
         nullable=False,
         server_default=text('true')
     )
+
+
+class User(Base):
+    __tablename__ = "user"
+    
+    id = Column(Integer, primary_key=True, nullable=False)
+    email = Column(String , nullable=False , unique=True)
+    password = Column(String , nullable=False)
+    created_at = Column(
+        TIMESTAMP(timezone=True),
+        nullable=False,
+        server_default=text('now()')
+    )
