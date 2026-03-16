da3599474c53650cefe13c1e725185f605396b03
Muskan Chauhan <chauhanmuskan291980@gmail.com>
2026-03-16 21:39:37 +0530

    Add response model to login endpoint for improved API documentation

--- app/router/auth.py
+++ app/router/auth.py
@@ -13,8 +13,8 @@ def get_db():
     finally:
         db.close()
 
-@router.post('/login')
-def login(user_credentials:OAuth2PasswordRequestForm = Depends() ,db:Session  = Depends(get_db)):
+@router.post('/login' , response_model=schemas.Token)
+def login(user_credentials:OAuth2PasswordRequestForm = Depends() ,db:Session  = Depends(get_db) ,):
     user = db.query(models.User).filter(models.User.email == user_credentials.username).first()
     if not user:
         raise HTTPException(status_code=status.HTTP_403_FORBIDDEN,
